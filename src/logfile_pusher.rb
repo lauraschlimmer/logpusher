6f29ed04b3b644ad1d74836ad2a6fd469034d44c
Laura Schlimmer <laura@zscale.io>
2017-11-13 15:40:50 +0100

    store lines in batches and process with multiple threads

--- src/logfile_pusher.rb
+++ src/logfile_pusher.rb
@@ -1,6 +1,7 @@
 class LogfilePusher
 
-  INSERT_BATCH_SIZE = 1024
+  INSERT_BATCH_SIZE = 2 #FIXME
+  NUM_THREADS = 3
 
   def initialize(logfile, regex, uploader, quiet=false)
     @logfile = logfile
@@ -8,6 +9,10 @@ class LogfilePusher
     @uploader = uploader
     @quiet = quiet
     @stats = UploadStats.new
+    @queue = Array.new
+    @complete = false
+    @mutex = Mutex.new
+    @cv = ConditionVariable.new
 
     if !File.file?(@logfile)
       raise "the file doesn't exist: #{@logfile}"
@@ -21,30 +26,30 @@ public
     cur_inode = nil
     cur_size = nil
 
-    while true
+    #while true FIXME
       # if the inode number of the file changes (e.g. the file has been
       # logrotated), start reading the file at the beginning
       if (inode = File.stat(@logfile).ino) != cur_inode
         cur_size = File.size(@logfile)
         cur_inode = inode
-        offset = upload_file(0)
+        offset = run_(0)
 
       # if the file size is smaller, start reading at the beginning
       elsif (size = File.size(@logfile)) < cur_size
         cur_size = size
-        offset = upload_file(0)
+        offset = run_(0)
 
       # if the file is bigger, start reading at the offset
       elsif size > cur_size
         cur_size = size
-        offset = upload_file(offset)
+        offset = run_(offset)
 
       else
         print_stats("wait")
       end
 
       sleep(5)
-    end
+    #end
 
   end
 
@@ -55,80 +60,109 @@ public
 
 private
 
-  # read the @logfile starting at position offset and push the loglines
-  #
-  # return the last position of the file
-  def upload_file(offset = 0)
-    buffer = []
-    idx = 0
-    File.open(@logfile) do |file|
-      while (chunk = file.read(1))
-        if (idx += 1) < offset
-          next
-        end
-        buffer.push(chunk)
-
-        if buffer.size > INSERT_BATCH_SIZE
-          buffer = processBuffer(buffer)
-        end
+  def run_(offset = 0)
+    # start threads
+    threads = Array.new(NUM_THREADS) do
+      Thread.new do
+        run_thread()
       end
     end
 
-    if buffer.size > 0
-      processBuffer(buffer)
+    # read and enqueue lines in batches
+    batch = Array.new
+    idx = 0
+    IO.readlines(@logfile).each do |line|
+      if (idx += 1) < offset
+        next
+      end
+
+      m = line.match(@regex)
+      if !m
+        $stderr.puts  "Dropping logline '#{line}' because it does not match the parsing regex: #{@regex}"
+        next
+      end
+
+      if batch.size >= INSERT_BATCH_SIZE
+        enqueue_batch(batch)
+        batch.clear()
+      end
+
+      batch.push(m)
+
+      print_stats("upload")
     end
+
+    if batch.size > 0
+      enqueue_batch(batch)
+    end
+
+    # send upload complete signal
+    @mutex.synchronize {
+      @complete = true
+      @cv.signal
+    }
+
+    # join threads
+    threads.each { |th| th.join }
 
     return idx
   end
 
-  # build up lines from the buffer, a line ends with \n, and push these lines
-  #
-  # if the last line is not complete, that is the buffer doesn't end with a line
-  # ending, an array with the remaining characters will be returned.
-  # Otherwise an empty array will be returned.
-  def processBuffer(buffer)
-    lines = []
-    line = ""
-    buffer.each do |c|
-      if c != "\n"
-        line += c
-        next
+  def enqueue_batch(batch)
+    @mutex.synchronize {
+      @queue.push(batch)
+      @cv.signal
+    }
+  end
+
+  def pop_batch()
+    @mutex.synchronize {
+      loop do
+        if @queue.size != 0
+          break;
+        end
+
+        if @complete
+          return nil
+        else
+          @cv.wait(@mutex)
+        end
       end
 
-      if !m = line.match(@regex)
-        $stderr.puts  "Dropping logline '#{line}' because it does not match the parsing regex: #{@regex}"
-      else
-        lines.push(m)
-      end
+      batch = @queue.shift
+      @cv.signal
+      return batch
+    }
+  end
 
-      line.clear()
+  def run_thread()
+    while (batch = pop_batch)
+      @uploader.insert(batch)
+      @stats.add_upload(batch.size)
+      print_stats("upload")
     end
-
-    @uploader.insert(lines)
-
-    @stats.add_upload(lines.size)
-    print_stats("upload")
-
-    return line.split("")
   end
 
   def print_stats(state)
     return if @quiet
 
-    $stderr.flush
-    case state
-    when "upload"
-      $stderr.print "Uploading... rate: #{@stats.get_rolling_rows_per_second} lines per second\r"
+      case state
+      when "upload"
+        @mutex.synchronize {
+          $stderr.print "Uploading... rate=#{@stats.get_rolling_rows_per_second}lines/s\r"
+        }
 
-    when "wait"
-      $stderr.print "Waiting... the file hasn't changed since the last upload\r"
+      when "wait"
+        $stderr.print "Waiting... the file hasn't changed since the last upload\r"
 
-    when "stop"
-      $stderr.print "Uploaded #{@logfile}..." +
-          "total: #{@stats.get_total_row_count} lines, " +
-          "runtime: #{@stats.get_total_runtime.round(2)}s\n"
+      when "stop"
+        $stderr.print "Uploaded #{@logfile}... " +
+            "total=#{@stats.get_total_row_count}lines, " +
+            "runtime=#{@stats.get_total_runtime.round(2)}s\n"
 
-    end
+      end
+
+      $stderr.flush
   end
 
 end
--- src/upload_stats.rb
+++ src/upload_stats.rb
@@ -6,23 +6,30 @@ class UploadStats
     @rolling_rows_per_second = RollingAggregate.new
     @total_row_count = 0
     @start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    @mutex = Mutex.new
   end
 
   def add_upload(num_rows, size_rows = 0)
-    @rolling_rows_per_second.add_value(num_rows)
-    @total_row_count += num_rows
+    @mutex.synchronize {
+      @rolling_rows_per_second.add_value(num_rows)
+      @total_row_count += num_rows
+    }
   end
 
   def get_rolling_rows_per_second
-    aggr = @rolling_rows_per_second.compute_aggregate
-    if aggr.interval_us == 0
-      return aggr.value
-    end
-    return Float(aggr.value) / Float(aggr.interval_us)
+    @mutex.synchronize {
+      aggr = @rolling_rows_per_second.compute_aggregate
+      if aggr.interval_us == 0
+        return aggr.value
+      end
+      return Float(aggr.value) / Float(aggr.interval_us)
+    }
   end
 
   def get_total_row_count
-    return @total_row_count
+    @mutex.synchronize {
+      return @total_row_count
+    }
   end
 
   def get_total_runtime
